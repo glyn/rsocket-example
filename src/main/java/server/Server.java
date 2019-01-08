cd9a151be48c1ff7472854cbe600186fe7478eff
Glyn Normington <gnormington@pivotal.io>
2019-01-08 13:21:45 +0000

    Add license

--- src/main/java/server/Server.java
+++ src/main/java/server/Server.java
@@ -1,3 +1,19 @@
+/*
+ * Copyright 2019 The original author or authors
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ *     http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+
 package server;
 
 import io.rsocket.*;
@@ -8,7 +24,6 @@ import reactor.core.publisher.Flux;
 import reactor.core.publisher.Mono;
 
 public class Server {
-
 	public static void main(String[] args) {
 		RSocketFactory.receive()
 				// .frameDecoder(Frame::retain) // Enable zero copy
