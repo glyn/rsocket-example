f663d85501fc7ef806bf35433ed07d10b622f562
Glyn Normington <gnormington@pivotal.io>
2019-01-10 17:05:44 +0000

    Copy logging properties into docker container

--- docker/push.sh
+++ docker/push.sh
@@ -3,4 +3,4 @@
 SCRIPT_DIR="$( cd "$( dirname "${BASH_SOURCE[0]}" )" >/dev/null 2>&1 && pwd )"
 
 cp ${SCRIPT_DIR}/../build/libs/rsocket-example.jar ${SCRIPT_DIR}/server/.
-cp -r ${SCRIPT_DIR}/../dependencies/*.jar ${SCRIPT_DIR}/server/dependencies
+cp ${SCRIPT_DIR}/../dependencies/* ${SCRIPT_DIR}/server/dependencies
